18687c4d2511f853308e1b6cff74c9d02c1b9966
Ivan Kurikhin <ivkurikhin@edu.hse.ru>
2023-12-05 19:48:14 +0300

    Complete standard monads classroom

---
+++ task10/task10.1.hs
@@ -0,0 +1,30 @@
+-- Рукописный логгер
+data Logged a = Logged String a deriving (Eq,Show)
+
+instance Functor Logged where
+  fmap f (Logged s a) = Logged s (f a)
+
+instance Applicative Logged where
+  pure = Logged ""
+  (<*>) (Logged s1 f) (Logged s2 a) = Logged (s1 ++ s2) (f a)
+
+instance Monad Logged where
+  return = pure
+  (>>=) (Logged s a) f = Logged (s' ++ s) y
+                        where
+                            Logged s' y = f a
+
+-- эквивалент tell
+write2log :: String -> Logged ()
+write2log str = Logged str ()
+
+logIt v = do
+    write2log $ "var = " ++ show v ++ "; "
+    return v
+
+test = do
+    x <- logIt 3
+    y <- logIt 5
+    let res = x + y
+    write2log $ "sum = " ++ show res ++ "; "
+    return res
